9ddff5b7f75e3a63cd0aa4c80b9119ca17eb0f21
Marek Valko <valko.marek@gmail.com>
2020-03-16 11:54:09 +0100

    Fixed getting more than 50 videos in playlist

--- server.js
+++ server.js
@@ -6,19 +6,21 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
 const MAX_RESULTS = 50;
+const VIDEO_IDS_LENGTH = 50;
 
 const YOUTUBE_PLAYLIST_URL =
     "https://www.googleapis.com/youtube/v3/playlistItems";
 const YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos";
-const YOUTUBE_PLAYLIST_INFO_URL = "https://www.googleapis.com/youtube/v3/playlists"
+const YOUTUBE_PLAYLIST_INFO_URL =
+    "https://www.googleapis.com/youtube/v3/playlists";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("/api/playlist", async (req, res) => {
-    const path = require('path')
     // return res.status(400).send({error:"random error"})
+    // const path = require("path");
     // return res.sendFile(path.join(__dirname,'playlist.json'))
     const youtube_id = req.query.youtube_id;
     const playlist_url = `${YOUTUBE_PLAYLIST_URL}?part=contentDetails,snippet&maxResults=${MAX_RESULTS}&playlistId=${youtube_id}&key=${API_KEY}`;
@@ -40,33 +42,50 @@ app.get("/api/playlist", async (req, res) => {
         }
         videos = [...videos, ...data.data.items];
     }
-    const ids = videos.map(video => video.contentDetails.videoId).join(",");
-    const videos_url = `${YOUTUBE_VIDEO_URL}?part=contentDetails,snippet,id&id=${ids}&key=${API_KEY}`;
-    let detail_videos = null;
-    try {
-        detail_videos = await axios.get(videos_url);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-    const results = detail_videos.data.items.map(video => {
-        return {
-            id: video.id,
-            title: video.snippet.title,
-            description: video.snippet.description,
-            thumbnailUrl: video.snippet.thumbnails.standard.url,
-            channelTitle: video.snippet.channelTitle,
-            duration: video.contentDetails.duration
-        };
-    });
+    const all_ids = videos.map(video => video.contentDetails.videoId);
+    // console.log(all_ids.join(','));
     
-    let playlistTitle = ""
-    try {
-        data = await axios.get(`${YOUTUBE_PLAYLIST_INFO_URL}?part=snippet&id=${youtube_id}&key=${API_KEY}`)
-        playlistTitle = data.data.items[0].snippet.title
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+    let chunked_ids = []; // API can resolve max 50 videos at a time, separating array into array of arrays of 50 ids
+    while (all_ids.length) {
+        chunked_ids.push(all_ids.splice(0, VIDEO_IDS_LENGTH));
     }
 
+    let results = [];
+    try {
+        results = await Promise.all(
+            chunked_ids.map(async ids => await Promise.all(
+                ids.map(async id => {
+                    const video_url = `${YOUTUBE_VIDEO_URL}?part=contentDetails,snippet,id&id=${id}&key=${API_KEY}`;
+                    const video_details = await axios.get(video_url);
+
+                    const video = video_details.data.items[0];
+                    return {
+                        id: video.id,
+                        title: video.snippet.title,
+                        description: video.snippet.description,
+                        thumbnailUrl: video.snippet.thumbnails.standard.url,
+                        channelTitle: video.snippet.channelTitle,
+                        duration: video.contentDetails.duration
+                    };
+                })
+            )))
+        
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+    results = results.flat()
+    
+
+
+    let playlistTitle = "";
+    try {
+        data = await axios.get(
+            `${YOUTUBE_PLAYLIST_INFO_URL}?part=snippet&id=${youtube_id}&key=${API_KEY}`
+        );
+        playlistTitle = data.data.items[0].snippet.title;
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
     return res.json({ playlistTitle, results });
 });
 
